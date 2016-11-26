69ec51c9a5f0901cbef1269c9675854a8728fd62
Giulia Tolomei <giuliatolomei@gmail.com>
2016-11-26 15:30:53 +0100

    [intial commit]

---
+++ es1.py
@@ -0,0 +1,3 @@
+list= ["lun", "mar", "mer", "gio", "ven", "sab", "dom"]*5
+for i in range (1, 31):
+	print (str(i) + " " +list[i-1]) 
---
+++ intro.py
@@ -0,0 +1,11 @@
+def prova (nome):
+	if nome=="Ale":
+		print("halo!")
+	elif nome=="Giulia":
+		print("ciao!")
+	else :
+		print("hello!")
+	
+prova ("Ale")
+prova ("Giulia")
+prova ("AAA")
